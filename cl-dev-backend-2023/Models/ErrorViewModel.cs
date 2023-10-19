e48cc9e5327d7ab3a984e76eb4e85427d3eee0ad
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2023-10-19 20:21:47 -0300

    Adicionar arquivos de projeto.

---
+++ cl-dev-backend-2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cl_dev_backend_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
