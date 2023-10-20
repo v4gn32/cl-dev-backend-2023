61baf45053c8af301e507fde409e35e2fb7a9843
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2023-10-19 21:45:26 -0300

    Configuração do Banco de Dados e da Tabela Veiculo

---
+++ cl-dev-backend-2023/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cl_dev_backend_2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrgiatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o ano de fabricação!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrgiatório informar o ano do modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
